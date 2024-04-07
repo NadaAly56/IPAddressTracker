2fc2ce3ccb211d89602816e305ef7d91b1266478
nada <nada.aly5613@gmail.com>
2024-04-07 16:53:51 +0200

    adding .env file

--- src/redux/IpAdressSlice.js
+++ src/redux/IpAdressSlice.js
@@ -5,9 +5,8 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";const initialSt
 };
 
 export const fetchData = createAsyncThunk("Ip/fetchData", async ({ ip }) => {
-  const res = await fetch(
-    `https://geo.ipify.org/api/v2/country,city?apiKey=at_eHUa2KiA6RcR4yrUjjEiMbx4V2VBy&ipAddress=${ip}`
-  );
+  console.log();
+  const res = await fetch(`${import.meta.env.VITE_API}${ip}`);
   return res.json();
 });
 const IpAdressSlice = createSlice({
